ee9f9933d31de4cd01fb2412f169e545eb467dab
Moham <Moham@MAHIS>
2023-12-05 21:37:17 +0100

    Add project files.

---
+++ Miniräknare/Operation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniräknare
+{
+    public class Operation
+    {
+        public int Result { get; set; } = 0;
+        public int NumberOperation(int firstNumber, int secondNumber, string tecken)
+        {
+            if (tecken == "+")
+            {
+
+                return firstNumber + secondNumber;
+            }
+            else if (tecken == "-")
+            {
+                return firstNumber - secondNumber;
+            }
+            else if (tecken == "/")
+            {
+                return firstNumber / secondNumber;
+            }
+            else if (tecken == "*" || tecken == "x" || tecken == "X")
+            {
+                return firstNumber * secondNumber;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Miniräknare/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Miniräknare;
+using System.ComponentModel.Design;
+
+Console.WriteLine("Välkommen till miniräknaren. Tryck vart som helst för att börja");
+Console.ReadLine();
+Console.WriteLine("Välj en siffra");
+string firstInput = Console.ReadLine();
+
+while (!int.TryParse(firstInput, out int number))
+{
+    Console.WriteLine("Välj en siffra som är ett hel tal");
+    firstInput = Console.ReadLine();
+}
+
+int firstNumber = Convert.ToInt32(firstInput);
+
+Console.WriteLine("välj sätt att beräkna");
+string tecken = Console.ReadLine();
+while (tecken != "+" && tecken != "-" && tecken != "*" && tecken != "/")
+{
+    Console.WriteLine("Var vänlig och välj något utav dom här tecknen: +, -, *, /");
+    tecken = Console.ReadLine();
+}
+
+Console.WriteLine("Välj din andra siffra");
+string secondInput = Console.ReadLine();
+
+
+while (!int.TryParse(secondInput, out int number))
+{
+    Console.WriteLine("Välj en siffra som är ett hel tal");
+    secondInput = Console.ReadLine();
+}
+
+int secondNumber = Convert.ToInt32(secondInput);
+
+
+Operation operation = new Operation();
+
+
+operation.Result = operation.NumberOperation(firstNumber, secondNumber, tecken);
+
+Console.WriteLine("Ditt svar är " + operation.Result);
+
+
+
+
+
